f9046570872bc3314ca6a761ed5cfed9f60731ed
atran1015 <aichantran1015@yahoo.com>
2022-07-14 14:43:18 -0700

    console log data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import TableDisplay from './components/displayTable.js'
 //import data from './assets/DOW.json'
 
 function App() {
-  
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello world!</p>
-        
         <TableDisplay/>
       </header>    
     </div>
--- src/components/displayTable.js
+++ src/components/displayTable.js
@@ -1,38 +1,29 @@
 import React, { useEffect } from "react";
 import jsondata from './DOW.json'
+import spdata from './SP500.json'
 import './table.css'
+
 
 function TableDisplay(){
 	//console.log(data,1); 
     const data = JSON.parse(JSON.stringify(jsondata));
     console.log(data);
- 
-    
+    console.log(spdata);
 	return(
         <div>
-            
-             
+            <table className="table">
+                <tr>
+                    <th>Stock Name 1</th>
+                    <th>Stock Name 2</th>
+                    <th>Stock Name 3</th>
+                </tr>
+                <tr>
+                    <td>Space Filler</td>
+                    <td>Space Filler</td>
+                    <td>Space Filler</td>
+                </tr>
+            </table>
         </div>
-	// 	<table>
-    //   <tbody>
-    //     <tr>
-    //       <td>
-    //         <h5>test</h5>
-    //       </td>
-    //       <td>
-    //         <h5>test</h5>
-    //       </td>
-    //       <td>
-    //         <h4>test</h4>
-    //       </td>
-    //       <td>
-    //         <p>test</p>
-    //       </td>
-    //     </tr>
-    //   </tbody>
-    // </table>
-    
-   
 	);
 }
 
